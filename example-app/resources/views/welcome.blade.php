53b88b48828806c400de489b05ea7a2eed58edcc
ThanadonMah <puun@Thanadons-ideapadGAMING3>
2023-08-03 19:08:19 +0700

    Updated

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.main')
-
 @section('content')
     <h1 class="text-3xl">
         Welcome to IceCreamHub
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -32,4 +32,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/userparticipate' , function () {
+    return view('userParticipate');
+});
+
 require __DIR__.'/auth.php';

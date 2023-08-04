afa404d2d866ba0ea1300febf930b241350909f2
ThanadonMah <puun@Thanadons-ideapadGAMING3>
2023-08-04 22:53:22 +0700

    Fix Create Event page

--- example-app/resources/views/createEvent.blade.php
+++ example-app/resources/views/createEvent.blade.php
@@ -2,23 +2,38 @@
 
 @section('content')
 <h1 class="text-3xl">Event Detail</h1>
-<div class="flex h4/5 p-2 py-10">
-    <div class="w-2/3">
-        <p class="">Event Title</p>
+<div class="flex h4/5 p-2 py-10 w-11/12">
+    <div class="w-3/4">
+        <p>Event Title</p>
+        <input required type="text" id="title" name="title" autocomplete="off"
+               placeholder="Put in event title"
+               class="border border-gray-300 shadow mb-4 px-5 pb-2.5 w-2/3 rounded-lg">
     </div>
-    <div class="w-1/3">
-        <p>Number of Staff</p>
+    <div class="w-1/4">
+        <p>Number of Staffs</p>
+        <input required type="text" id="numberOfStaff" name="numberOfStaff" autocomplete="off"
+               placeholder="Put in number of staffs"
+               class="border border-gray-300 shadow px-5 pb-2.5 w-full rounded-lg">
     </div>
 </div>
-<div class="flex justify-between mx-5 p-6 text-xl bg-white rounded-lg">
+<div class="p-6 text-xl bg-white h-96 w-11/12 rounded-lg">
     <ul>
-        <li class="text-xl font-medium mb-4 px-12 pb-10 ">Event description</li>
-        <li class="text-xl font-medium mb-4 p-10 ">.............</li>
-        <li class="text-xl font-medium mb-4 p-10 ">.............</li>
-        <li class="text-xl font-medium mb-4 p-10 ">.............</li>
+        <li class="text-xl font-medium mb-2 px-1 pb-2.5 ">Event description</li>
+        <input required type="text" id="description" name="description" autocomplete="off"
+               placeholder="Put in event description"
+               class="border border-gray-300 shadow px-3 pb-1 h-72 w-full rounded-lg align-top">
     </ul>
 </div>
-<div class="flex flex-row-reverse py-5">
-    <button class="bg-light-blue w-1/4 p-4 rounded-full">Create Event</button>
+<div class="flex h4/5 p-2 py-10 w-11/12">
+    <div class="w-2/3">
+        <p>Event Title</p>
+        <input required type="text" id="budget" name="budget" autocomplete="off"
+               placeholder="Put in event bedget"
+               class="border border-gray-300 shadow mb-4 px-5 pb-2.5 w-2/3 rounded-lg">
+    </div>
+    <div class="flex flex-row-reverse py-5 w-2/3">
+        <button class="bg-light-blue w-1/4 p-4 rounded-full">Create Event</button>
+    </div>
 </div>
+
 @endsection

58db3ae7b4e361b0f968845431eb55a51f1619b2
ThanadonMah <puun@Thanadons-ideapadGAMING3>
2023-08-04 23:31:19 +0700

    New Route for staffDashboard.blade.php in web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -26,12 +26,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/userparticipate' , function () {
     return view('userParticipate');
 });
@@ -47,5 +41,17 @@ Route::get('/myevent' , function () {
 Route::get('/createevent' , function () {
     return view('createEvent');
 });
+
+Route::get('/staffdashboard' , function () {
+    return view('staffDashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
 
 require __DIR__.'/auth.php';

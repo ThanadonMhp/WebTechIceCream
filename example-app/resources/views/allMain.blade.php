df77b36c95612f1104045ec8520af903647f97ec
ThanadonMah <puun@Thanadons-ideapadGAMING3>
2023-08-04 23:30:20 +0700

    Fix Main Screen Page

--- example-app/resources/views/allMain.blade.php
+++ example-app/resources/views/allMain.blade.php
@@ -2,69 +2,35 @@
 
 @section('content')
 <h1 class="text-4xl mb-6 py-3 pl-12"><strong>Events</strong></h1>
+
 <div class=" justify-between mb-4 p-6 border-b-2 text-xl bg-white rounded-lg">
     <div class="">
-        <div>
-            <p class="">Event Title1</p>
-            <div class="">
-                <ul>
-                    <li class="text-xl font-medium px-12">Event description</li>
-                    <li class="text-xl font-medium px-12">.............</li>
-                </ul>
-            </div>
-        </div>
+        <p class="">Event Title1</p>
+            <ul>
+                <li class="text-xl font-medium px-12">Event description</li>
+                <li class="text-xl font-medium px-12">.............</li>
+            </ul>
     </div>
 </div>
+
 <div class=" justify-between mb-4 p-6 border-b-2 text-xl bg-white rounded-lg">
     <div class="">
-        <div>
-            <p class="">Event Title2</p>
-            <div class="">
-                <ul>
-                    <li class="text-xl font-medium px-12">Event description</li>
-                    <li class="text-xl font-medium px-12">.............</li>
-                </ul>
-            </div>
-        </div>
+        <p class="">Event Title2</p>
+        <ul>
+            <li class="text-xl font-medium px-12">Event description</li>
+            <li class="text-xl font-medium px-12">.............</li>
+        </ul>
     </div>
 </div>
+
 <div class=" justify-between mb-4 p-6 border-b-2 text-xl bg-white rounded-lg">
     <div class="">
-        <div>
-            <p class="">Event Title3</p>
-            <div class="">
-                <ul>
-                    <li class="text-xl font-medium px-12">Event description</li>
-                    <li class="text-xl font-medium px-12">.............</li>
-                </ul>
-            </div>
-        </div>
+        <p class="">Event Title3</p>
+        <ul>
+            <li class="text-xl font-medium px-12">Event description</li>
+            <li class="text-xl font-medium px-12">.............</li>
+        </ul>
     </div>
 </div>
-<div class=" justify-between mb-4 p-6 border-b-2 text-xl bg-white rounded-lg">
-    <div class="">
-        <div>
-            <p class="">Event Title4</p>
-            <div class="">
-                <ul>
-                    <li class="text-xl font-medium px-12">Event description</li>
-                    <li class="text-xl font-medium px-12">.............</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
-<div class=" justify-between mb-4 p-6 border-b-2 text-xl bg-white rounded-lg">
-    <div class="">
-        <div>
-            <p class="">Event Title5</p>
-            <div class="">
-                <ul>
-                    <li class="text-xl font-medium px-12">Event description</li>
-                    <li class="text-xl font-medium px-12">.............</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
+
 @endsection

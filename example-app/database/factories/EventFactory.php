016cc106b0be6def6d675a4358be9f1ab9edccae
oOTEEOo <thanadon.kr@ku.th>
2023-08-09 00:18:11 +0700

    create factory

---
+++ example-app/database/factories/EventFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Event>
+ */
+class EventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'event_name' => fake()->name(),
+            'event_id' => fake()->randomNumber(),
+            'budget' => fake()->randomNumber(),
+            'size' => fake()->randomNumber()
+        ];
+    }
+}

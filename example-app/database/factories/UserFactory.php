aaa7197f06d5bcc24f9c062278fe7e2a1b7288e9
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2023-08-10 00:39:14 +0700

    New file for database.

--- example-app/database/factories/UserFactory.php
+++ example-app/database/factories/UserFactory.php
@@ -22,6 +22,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'year' => fake()->numberBetween(1,8),
+            'certificate' => fake()->realTextBetween(10, 20, 5),
+
             'remember_token' => Str::random(10),
         ];
     }

5beb0c72ddbba4fbe0cea9d43abf3c274dfedf2f
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2023-08-21 18:43:44 +0700

    Add profile Picture to navbar.blade.php

--- example-app/resources/views/layouts/subviews/navbar.blade.php
+++ example-app/resources/views/layouts/subviews/navbar.blade.php
@@ -20,7 +20,7 @@
                             class="nav-menu {{ Route::currentRouteName() === 'events.pending' ? 'active' : '' }}">
                                 <strong>Pending Events</strong>
                             </a>
-                        </li>                        
+                        </li>
                     @else
                         <li class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             <a href="{{ route('events.myevents') }}"
@@ -35,20 +35,29 @@
                             </a>
                         </li>
                     @endif
-                    
+
                 @endif
-                
+
             </ul>
         </div>
 
         @if( Auth::check() )
         <div class="flex items-center">
-            <div class="mr-4">
+            <div class="w-1/2 pr-4 flex flex-col items-center w-16">
+                <a href="{{ route('users.show', ['user' => Auth::user() ]) }}">
+                    @if( Auth::user()->imgPath !== null )
+                    <img src="{{ asset('storage/' . Auth::user()->imgPath) }}" alt="Profile Picture" class="max-w-full h-3/4 object-contain rounded-full">
+                    @else
+                    <img src="/images/defaultProfile.jpeg" alt="Profile Picture" class="max-w-full h-3/4 object-contain rounded-full">
+                    @endif
+                </a>
+            </div>
+            <div class="mr-4 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                 <a href="{{ route('users.show', ['user' => Auth::user() ]) }}">
                     {{ Auth::user()->name }}
                 </a>
             </div>
-            <div class="text-gray-800 bg-indigo-100 hover:bg-indigo-200 inline-flex items-center justify-center px-3 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm ">
+            <div class="text-gray-800 bg-indigo-100 hover:bg-indigo-200 inline-flex items-center justify-center px-3 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm h-12">
                 <form action="{{ route('logout') }}"method='POST'>
                     @csrf
                     <button type="submit">Logout</button>

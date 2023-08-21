5aa2c0cd8696bc51e0dd9d2382b6847f65bf282d
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2023-08-21 15:54:55 +0700

    New account for IceCream Group Member.

--- example-app/database/seeders/UserSeeder.php
+++ example-app/database/seeders/UserSeeder.php
@@ -16,6 +16,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make("password");
+        $user->role = "ADMIN";
+        $user->save();
+
+        $user = new User();
         $user->name = 'Test_Account01';
         $user->email = 'test01@gmail.com';
         $user->password = Hash::make("password");
@@ -31,12 +38,25 @@ class UserSeeder extends Seeder
 
         $user = User::factory()->count(50)->create();
 
+        $user = new User();
+        $user->name = 'BoostChavit';
+        $user->email = 'chavit.si@ku.th';
+        $user->password = Hash::make("password");
+        $user->year = 3;
+        $user->save();
 
         $user = new User();
-        $user->name = 'Admin';
-        $user->email = 'admin@gmail.com';
+        $user->name = 'oOTEEOo';
+        $user->email = 'thanadon.kr@ku.th';
         $user->password = Hash::make("password");
-        $user->role = "ADMIN";
+        $user->year = 3;
+        $user->save();
+
+        $user = new User();
+        $user->name = 'ThanadonMhp';
+        $user->email = 'thanadon.mah@ku.th';
+        $user->password = Hash::make("password");
+        $user->year = 3;
         $user->save();
     }
 }
